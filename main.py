ea4937dfcc1d6be653bd4e60a41f5540b6edaa82
usamaSadiq-26 <theusamasadiq@gmail.com>
2025-12-19 22:50:43 +0500

    day 12 of 100 days of python programming

---
+++ main.py
@@ -0,0 +1,76 @@
+# print('hey this is \"100days python code\" challenge\nand i am usama, your python teacher')
+
+
+# a = 2000
+# b = "Ladin"
+# c = False
+# d = None
+# a1 = 2
+
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(a,b,c,d)
+# print(a+a1)
+# print(a-a1)
+# print(a/a1)
+# print(a*a1)
+# print("The type of a is", type(a))
+# print("The type of a is", type(b))
+# print("The type of a is", type(c))
+# print("The type of a is", type(d))
+
+
+#day06
+# list1 = [1231,"LADIN",["WILL BE A PROFESSIONAL PYTHON DEVELOPER IN",2026]]
+# print(list1)
+
+# tuple1 = ((123,321),("LADIN",1232))
+# print(tuple1)
+
+# dict1 = {"Full name":"Usama Sadiq","Username":"LADIN","Age":25, "Sex":"Male", "Can Vote":True}
+# print(dict1)
+
+
+#day07
+# print(15+6) #Addition Operator
+# print(15-6) #Subtraction Operator
+# print(15*6) #Multiplication Operator
+# print(15/6) #Division Operator
+# print(15//6) #Modulus Operator
+# print(5%3) #Floor Division Operator
+# print(5**3) #Exponential Operator
+
+#HOW TO MAKE A CALCULATOR BY USING PYTHON
+
+# a = 5
+# b = 3
+
+# ans1 = a+b
+# print("Addition of",a, "and",b,"is equals to", ans1)
+
+# ans2 = a-b
+# print("Subraction of",a, "and",b,"is equals to", ans2)
+
+# ans3 = a*b
+# print("Multiplication of",a, "and",b,"is equals to", ans3)
+
+# ans4 = a/b
+# print("Division of",a, "and",b,"is equals to", ans4)
+
+# ans5 = a//b
+# print("Modulus of",a, "and",b,"is equals to", ans5)
+
+# ans6 = a%b
+# print("Floor Division of",a, "and",b,"is equals to", ans6)
+
+# ans7 = a**b
+# print("Exponential poewr of",b, "on",a,"is equals to", ans7)
+
+
+#day08
+#calculator solution explained
+
+#day09
